eb3ed7e75ab33c74b85de4db25e51a1fbee8eb5c
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-05-18 12:09:37 +0200

    Loopa över fem dagar

--- tidsrapportering.bash
+++ tidsrapportering.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Det här skriptet kommer försöka fylla i precis samma tider varje dag under fyra veckors tid i FLEX HRM."
+echo "Det här skriptet kommer försöka fylla i precis samma tider varje dag, fem dagar i streck, i FLEX HRM."
 echo "Det räknar med att du kör Firefox med tillägget Vim Vixen."
 echo "Det räknar också med att du inte visar bokmärkesmenyn."
 echo
@@ -10,15 +10,19 @@ echo "Markera Firefox-fönstret med FLEX öppnat och navigerat till dagsvyn för
 firefox=$(xdotool selectwindow)
 echo "Det valda fönstret har titel " $(xdotool getwindowname $firefox)
 echo
-echo "Fyller i tiderna för en dag."
-xdotool key       --window $firefox --delay 75 f a j
-xdotool windowfocus $firefox
-xdotool type      --window $firefox 'ARB'
-xdotool key       --window $firefox Tab 8 Tab 1 2
-echo "Sparar sida"
-sleep 0.5
-xdotool key       --window $firefox --delay 100 F6 Tab Tab Tab Tab f j
-echo "Väntar på att skiten ska ladda om innan vi får gå till nästa dag"
-sleep 3.5
-echo "Går till nästa dag"
-xdotool key       --window $firefox --delay 75 f a i
+for dag in {1..5}; do
+	echo "Fyller i tiderna för en dag."
+	xdotool key       --window $firefox --delay 100 f a k
+	xdotool windowfocus $firefox
+	xdotool type      --window $firefox 'ARB'
+	sleep 0.7
+	xdotool key       --window $firefox --delay 200 Tab 6 Tab 1 2
+	echo "Sparar sida."
+	sleep 0.5
+	xdotool key       --window $firefox --delay 100 F6 Tab Tab Tab Tab f k
+	echo "Väntar på att skiten ska ladda om innan vi får gå till nästa dag."
+	sleep 3.5
+	echo "Går till nästa dag."
+	xdotool key       --window $firefox --delay 100 f a j
+	sleep 4
+done
